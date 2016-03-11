d88f18812b21969a1bd75632963c8dd4f48bed19
Rossen Georgiev <rossen@rgp.io>
2016-03-11 01:10:35 +0000

    added match and player features

--- csgo/features/__init__.py
+++ csgo/features/__init__.py
@@ -1,4 +1,7 @@
-class FeatureBase(object):
+from csgo.features.match import Match
+from csgo.features.player import Player
+
+class FeatureBase(Match, Player):
     """
     This object is used to all high level functionality to CSGOClient.
     The features are seperated into submodules with a single class.
