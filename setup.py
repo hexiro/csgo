cbf3cd3997741def256762b3991c8410108717fa
Rossen Georgiev <rossen@rgp.io>
2017-12-09 14:17:30 +0000

    use steam==0.8.15 where gc protobuf is fixed

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'csgo/__init__.py'), encoding='utf-8') as f:
     __version__ = f.readline().split('"')[1]
 
 install_requires = [
-    'steam>=0.8.13',
+    'steam>=0.8.15',
     'gevent-eventemitter>=2.0',
     'gevent>=1.1',
     'protobuf>=3.0.0',
